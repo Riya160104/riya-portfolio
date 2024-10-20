2b2e160e8ff852d12a18b673b6c902fa3cc53089
riya <riyaa.gupta1625@gmail.com>
2024-10-20 23:55:00 +0530

    added ui

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,20 +1,19 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Riya Gupta",
   roles: [
-    "Full Stack Developer",
-    "Android Developer",
-    "UI/UX Designer",
-    "Programmer",
+    "Software Engineer",
+    "Web Developer",
+    
   ],
   description:
-    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+    
+
+"I'm a highly motivated and adaptable software engineer, always ready to take on new technical challenges. With a strong passion for learning and improving my skills, I focus on writing clean, efficient code and delivering quality solutions. With a positive attitude and a growth-oriented mindset, I'm eager to contribute to impactful projects and drive innovation.",
+  github: "https://github.com/Riya160104",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+    "https://drive.google.com/file/d/1A4UqDGqcMXcSbi4ECWRWK9lrr63IVHVd/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/riya-gupta-645538283",
+  
 };
 
 export const skills = [
